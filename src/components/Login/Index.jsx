e339c084d82a4602a735428f7f5484a8e384a26c
Jessica Sousa <jessica.sousa@koin.com.br>
2024-08-30 17:22:25 -0300

    refactor page login

--- src/components/Login/Index.jsx
+++ src/components/Login/Index.jsx
@@ -1,29 +1,13 @@
 import styles from "./login.module.css";
-import Logo from "../../assets/logo/amarelo.png";
-import { ArrowLeft } from "phosphor-react";
 import { Link } from "react-router-dom";
 
 export function LoginUser() { 
   return (
-    <div className={styles.container}>
-      <div className={styles.colorbackground}>
+      <div className={styles.container}>
         <div className={styles.form1}>
           <div>
-            <img className={styles.imgLogo} src={Logo} />
+              <h1>LOGIN</h1>
           </div>
-          <h2>Não deixe para amanhã a cerveja que você pode comprar hoje!</h2>
-          <h3>
-            Faça seu Login e aproveite ofertas exclusivos e cupons de descontos.
-            Para Aproveitar nossos beneficíos
-            <br />
-          </h3>
-          <Link className={styles.link} to="/register">
-            <ArrowLeft size={24} />
-            Cadastre-se Aqui .
-          </Link>
-        </div>
-
-        <div className={styles.form2}>
           <div className={styles.custom}>
             <label className={styles.customLabel}>E-mail</label>
             <input className={styles.customInput} id="email" name="email" />
@@ -38,12 +22,12 @@ export function LoginUser() {
             />
           </div>
           <div className={styles.btn}>
-            <button className={styles.btnPortal} type="submit">
-              <strong>Entrar</strong>
-            </button>
+              <Link className={styles.btnPortal} to="/home">ENTRAR</Link>
           </div>
+              <h3>CRIE UMA CONTA</h3>
+              <h4>Ainda não tem uma conta no Portal do Malte?</h4>
+              <Link className={styles.btnPortal}  to="/register">CADASTRE-SE</Link>
         </div>
       </div>
-    </div>
   );
 }
